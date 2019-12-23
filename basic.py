73cd27f7f11798635b90f9331c16bcc2908ab687
redopm <omprakashmaury001#gmail.com>
2019-12-24 02:00:10 +0530

    basic command

--- basic.py
+++ basic.py
@@ -1,4 +1,15 @@
 from selenium import webdriver
-browser= webdriver.Firefox()
-browser.get("http://demo.automationtesting.in/Windows.html")
-browser.quit()
+#from selenium.webdriver.common.keys import key
+import time 
+
+driver = webdriver.Firefox()
+driver.get("http://demo.automationtesting.in/Windows.html")
+print(driver.title) # return the title of page 
+print(driver.current_url)# return corrent url of page 
+#print(driver.page_source) # return html format page source 
+
+driver.find_element_by_xpath('//*[@id="Tabbed"]/a/button').click() # click on button to open child page
+
+time.sleep(10)
+driver.close() # close the all window  insteed of current 
+#driver.quit() #  close browser
