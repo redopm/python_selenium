2b295fca549a9a66e0d9c63220124b32276befe8
redopm <omprakashmaury001#gmail.com>
2019-12-24 23:34:22 +0530

    2nd days

---
+++ conditioncomm.py
@@ -0,0 +1,6 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+driver = webdriver.Firefox()
+
+driver.get("http://www.demo.guru99.com/V4/manager/addcustomerpage.php")
