85e366ab9bf6fdae051e37615681275c3ca96937
redopm <omprakashmaury001#gmail.com>
2019-12-27 20:51:14 +0530

    condition

--- conditioncomm.py
+++ conditioncomm.py
@@ -1,6 +1,11 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+import time
 
 driver = webdriver.Firefox()
 
-driver.get("http://www.demo.guru99.com/V4/manager/addcustomerpage.php")
+driver.get("http://www.demo.guru99.com/V4/index.php")
+time.sleep(5)
+
+user_ele = driver.find_elements_by_xpath("/html/body/form/table/tbody/tr[1]/td[2]/input").send_Keys("opm")
+driver.quit() 
